7bae9975a78b0d4131dae28464b3c7e125acc077
esther <esthercar@leader.codes>
2021-05-27 13:17:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import MyChats from './components/myChats'
+import  ChatCard  from "./components/chatCard";
+// import Chat from './fromTovi/chat'
 import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <MyChats> */}
+          <ChatCard></ChatCard>
+        {/* </MyChats> */}
+        {/* <Chat></Chat> */}
       </header>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/myChats.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const myChats = () => {
+    return (
+        <div>
+            <h1>chat</h1>
+            <div className="chatBox" id='chatBox'>
+
+            </div>
+        </div>
+    )
+}
+
+export default myChats;
